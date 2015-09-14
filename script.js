b9b008ec2327b510d44fa0ce7579de162b61f1c8
Michael Dominic Bejar <dominic@aiming-inc.com.ph>
2015-09-14 16:27:19 +0800

    added index.html and script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+
+angular.module("MainApp",[]).controller("MainController", function($scope){
+  $scope.message = "yeah";
+  $scope.datetoday = "SEPt 14";
+}).controller("DateController", function($scope){
+
+  $scope.date = "yun oh";
+});
+
+
